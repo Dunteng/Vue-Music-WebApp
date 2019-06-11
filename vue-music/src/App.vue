4cddee0b97f30232f639012e092b054c93961981
Dunteng <lindunteng@gmail.com>
2019-06-11 15:21:38 +0800

    master init

---
+++ vue-music/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>你好吗！</h1>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  #app
+    color: $color-theme
+</style>
